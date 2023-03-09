630e329be04543a862985a95f5de2023098b5f81
Roberto Carlos López Abarca <rcla200031@gmail.com>
2023-03-09 17:38:26 -0600

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationpractica06_dsm104/LinearLayout.kt
@@ -0,0 +1,16 @@
+package com.example.myapplicationpractica06_dsm104
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LinearLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout)
+    }
+
+    fun finalizar(view: View?) {
+        finish()
+    }
+}
